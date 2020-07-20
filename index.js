4db5af7aa45380d88bbad023c05e2c43b0615960
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-07-20 21:17:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+
+function writeCards(arr) {
+    let output = "";
+    for (let i = 0; i < arr.length; i++) {
+        output += "Thank you, ${arr[i]} for the wonderful birthday gift!";
+    }
+    return output;
+}
+
+let names = ["Ada", "Brendan", "Ali"];
+console.log(writeCards(names));
